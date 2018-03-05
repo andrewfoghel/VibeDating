9c810f61d4cf88578bce7572c2558e6cea06cf1e
Andrew <you@example.com>
2018-03-05 00:29:25 -0700

    different image picker

--- VibeDating/CustomImagePicker/PhotoSelectorController.swift
+++ VibeDating/CustomImagePicker/PhotoSelectorController.swift
@@ -38,15 +38,17 @@ class PhotoSelectorController: UIViewController {
         print("Add Image")
     }
     
-    var selectedImageView = RoundImageView(color: .purple, cornerRadius: 10)
+  //  var selectedImageView = RoundImageView(color: .purple, cornerRadius: 10)
     
     fileprivate func setupViews() {
-        view.addSubview(selectedImageView)
-        selectedImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: nil, right: nil, bottom: nil, paddingTop: 24, paddingLeft: 0, paddingRight: 0, paddingBottom: 0, width: 350, height: 350)
-        selectedImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    //    view.addSubview(selectedImageView)
+    //    selectedImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: nil, right: nil, bottom: nil, paddingTop: 24, paddingLeft: 0, paddingRight: 0, paddingBottom: 0, width: 350, height: 350)
+    //    selectedImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
         view.addSubview(scrollView)
-        scrollView.anchor(top: selectedImageView.bottomAnchor, left: selectedImageView.leftAnchor, right: selectedImageView.rightAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, paddingTop: 12, paddingLeft: 0, paddingRight: 0, paddingBottom: 0, width: 0, height: 0)
+        scrollView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: nil, right: nil, bottom: view.safeAreaLayoutGuide.bottomAnchor, paddingTop: 34, paddingLeft: 0, paddingRight: 0, paddingBottom: 24, width: 350, height: 0)
+        scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
         
     }
     
@@ -58,6 +60,8 @@ class PhotoSelectorController: UIViewController {
         fetchPhotos()
     }
     
+    
+    var cells = [String : RoundImageView]()
     func setupGrid(size: Int) {
         let step: CGFloat = CGFloat(size / 4)
         DispatchQueue.main.async {
@@ -72,22 +76,83 @@ class PhotoSelectorController: UIViewController {
                 self.scrollView.addSubview(iv)
                 iv.contentMode = .scaleAspectFill
                 iv.image = self.images[i]
-            
+                
+                let key = "\(Int(rowCount))|\(Int(colCount))"
+                self.cells[key] = iv
             }
         }
     }
     
+    var selectedCell: RoundImageView?
+    
     @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
         let location = gesture.location(in: scrollView)
-        print(location)
+        let width = view.frame.width / 4
+        let i = Int(location.x / width)
+        let j = Int(location.y / width)
+        
+        let key = "\(j)|\(i)"
+        guard let imageView = cells[key] else { return }
+        let cellOriginalFrame = imageView.frame
+        if selectedCell != imageView {
+            if selectedCell == cells["0|0"] {
+                selectedCell?.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+            } else if selectedCell == cells["0|3"] {
+                selectedCell?.frame = CGRect(x: self.scrollView.frame.width - 80, y: 0, width: 80, height: 80)
+            } else if self.selectedCell == self.cells["6|0"] {
+                self.selectedCell?.frame = CGRect(x: 0, y: self.scrollView.frame.height - 90, width: 80, height: 80)
+            } else if self.selectedCell == self.cells["6|3"] {
+                self.selectedCell?.frame = CGRect(x: self.scrollView.frame.width - 80, y: self.scrollView.frame.height - 90, width: 80, height: 80)
+            } else {
+                selectedCell?.layer.transform = CATransform3DIdentity
+            }
+        }
+        
+        selectedCell = imageView
+        let center = imageView.center
+        scrollView.bringSubview(toFront: imageView)
+        var size: CGFloat = 80
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            if j == 0 && i == 0 {
+                imageView.frame = CGRect(x: 0, y: 0, width: 160, height: 160)
+            } else if j == 0 && i == 3 {
+                imageView.frame = CGRect(x: self.scrollView.frame.width - 160, y: 0, width: 160, height: 160)
+            } else if j == 6 && i == 0 {
+                imageView.frame = CGRect(x: 0, y: self.scrollView.frame.height - 170, width: 160, height: 160)
+            } else if j == 6 && i == 3 {
+                imageView.frame = CGRect(x: self.scrollView.frame.width - 170, y: self.scrollView.frame.height - 160, width: 160, height: 160)
+            } else if j == 0 {
+                let centerX: CGFloat = CGFloat((i * 80) + (i * 10) + 40)
+                imageView.frame = CGRect(x: centerX - 80, y: 0, width: 160, height: 160)
+            } else {
+                imageView.layer.transform = CATransform3DMakeScale(2, 2, 2)
+            }
+        }, completion: nil)
+        
+        if gesture.state == .ended {
+            UIView.animate(withDuration: 0.5, delay: 0.25, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+                if self.selectedCell == self.cells["0|0"] {
+                    self.selectedCell?.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+                } else if self.selectedCell == self.cells["0|3"] {
+                    self.selectedCell?.frame = CGRect(x: self.scrollView.frame.width - 80, y: 0, width: 80, height: 80)
+                } else if self.selectedCell == self.cells["6|0"] {
+                    self.selectedCell?.frame = CGRect(x: 0, y: self.scrollView.frame.height - 90, width: 80, height: 80)
+                } else if self.selectedCell == self.cells["6|3"] {
+                    self.selectedCell?.frame = CGRect(x: self.scrollView.frame.width - 80, y: self.scrollView.frame.height - 90, width: 80, height: 80)
+                } else if self.selectedCell == self.cells["0|\(i)"] {
+                    self.selectedCell?.frame = CGRect(x: CGFloat(i * 80) + CGFloat(i * 10), y: 0, width: 80, height: 80)
+                }else {
+                    self.selectedCell?.layer.transform = CATransform3DIdentity
+                }
+            }, completion: { (_) in
+        
+            })
+        }
     }
-    
-    
-    
     
     fileprivate func assetFetchOptions() -> PHFetchOptions {
         let fetchOptions = PHFetchOptions()
-        fetchOptions.fetchLimit = 32
+        fetchOptions.fetchLimit = 28
         let sortDescriptor = NSSortDescriptor(key: "creationDate", ascending: false)
         fetchOptions.sortDescriptors = [sortDescriptor]
         return fetchOptions
